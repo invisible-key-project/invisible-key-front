6dc035d3ebb83616cb62223e08251dc4239ea80a
eldeoddt <eldeoddt@gmail.com>
2024-04-12 19:31:58 +0900

    [ADD] Fragments 종속성 추가 #1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,4 +48,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
 }
