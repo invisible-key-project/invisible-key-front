fedba6ddfdcbbfd00c4e49109e7947a7feeafb0a
eldeoddt <eldeoddt@gmail.com>
2024-04-20 01:37:49 +0900

    [ADD] 코드 수정 #1

--- app/src/main/java/com/example/transparentkey_aos/EmbedPhotosFragment.kt
+++
@@ -1,8 +0,0 @@
-package com.example.transparentkey_aos
-
-import androidx.fragment.app.Fragment
-import com.example.transparentkey_aos.databinding.ActivityMainBinding
-
-class EmbedPhotosFragment: Fragment() {
-    //lateinit var binding:
-}
