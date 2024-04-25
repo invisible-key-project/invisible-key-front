15e7124c3412c0e28b948f5f98356dce7f219acc
eldeoddt <eldeoddt@gmail.com>
2024-04-25 23:25:32 +0900

    [ADD] 내비바 높이 조정 #8

--- app/src/main/java/com/example/transparentkey_aos/MainActivity.kt
+++ app/src/main/java/com/example/transparentkey_aos/MainActivity.kt
@@ -1,14 +1,10 @@
 package com.example.transparentkey_aos
 
-import android.Manifest
-import android.content.pm.PackageManager
 import android.content.res.ColorStateList
 import android.graphics.Color
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.transparentkey_aos.databinding.ActivityMainBinding
